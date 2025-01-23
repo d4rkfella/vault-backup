5011e43147a529bcab771f8366ef958ca2a7a276
Georgi Panov <77702912+d4rkfella@users.noreply.github.com>
2025-01-23 19:19:58 +0200

    Update backupVault.sh

--- backupVault.sh
+++ backupVault.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-export VAULT_TOKEN=$(&lt;/home/appuser/.aws/token)
+export VAULT_TOKEN=$(< /home/appuser/.aws/token)
 DATE=`date +%Y-%m-%d-%H-%M-%S`
 vault operator raft snapshot save /tmp/vaultsnapshot-$DATE.snap
-/usr/local/bin/aws s3 cp /tmp/vaultsnapshot-$DATE.snap s3://$S3BUCKET/
+
+/usr/local/bin/aws --endpoint-url $S3_ENDPOINT s3 cp /tmp/vaultsnapshot-$DATE.snap s3://$S3BUCKET/
+
 rm /tmp/vaultsnapshot-$DATE.snap
 echo "Completed the backup - " $DATE
