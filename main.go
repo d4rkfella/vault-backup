04bf4551e969ca2dcfaca94d19687311026ece6c
Georgi Panov <georgi.panov@darkfellanetwork.com>
2025-05-08 11:47:41 +0300

    Update main.go
    Signed-off-by: Georgi Panov <georgi.panov@darkfellanetwork.com>

--- main.go
+++ main.go
@@ -4,12 +4,13 @@ import (
 	"context"
 	"os"
 	"os/signal"
+	"syscall"
 
 	"github.com/d4rkfella/vault-backup/cmd"
 )
 
 func main() {
-	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer stop()
 
 	cmd.ExecuteContext(ctx)
